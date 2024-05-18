121580ce367cf047d236c5060bb548f45142e3cc
Adones Balica <adonesbalica@gmail.com>
2024-05-18 17:18:23 -0300

    Cadastro de usuario

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customerAlredyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlredyExists) {
+        return res.status(400).json({ error: 'Customer alredy exists!' });
+    }
+
+    customers.push({ cpf, name, id: uuidv4(), statement: [] });
+
+    return res.status(201).send();
+});
 
 app.listen(8181, () => console.log('Server is Running'));
