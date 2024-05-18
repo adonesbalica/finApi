102c56e4932fceb998513775faaf4d6a2965794c
Adones Balica <adonesbalica@gmail.com>
2024-05-18 17:23:14 -0300

    Rota de conferir o extrato bancario

--- src/index.js
+++ src/index.js
@@ -21,4 +21,12 @@ app.post('/account', (req, res) => {
     return res.status(201).send();
 });
 
+app.get('/statement/:cpf', (req, res) => {
+    const { cpf } = req.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return res.json(customer.statement);
+});
+
 app.listen(8181, () => console.log('Server is Running'));
